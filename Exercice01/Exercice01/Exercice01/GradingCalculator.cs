5b3adaa1a7005574837dda532ad59c7d69d70530
Zakaria Z. <zakaria@m2i.com>
2026-02-25 13:42:16 +0100

    Exercice 01 - Bases

---
+++ Exercice01/Exercice01/Exercice01/GradingCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice01
+{
+    public class GradingCalculator
+    {
+        public int Score { get; set; }
+        public int AttendancePercentage { get; set; }
+
+        public char GetGrade()
+        {
+            if (Score > 90 && AttendancePercentage > 70) return 'A';
+            else if (Score > 80 && AttendancePercentage > 60) return 'B';
+            else if (Score > 60 && AttendancePercentage > 60) return 'C';
+            else return 'F';
+        }
+    }
+}
---
+++ Exercice01/Exercice01/Exercice01Test/GradingCalculatorTest.cs
@@ -0,0 +1,100 @@
+﻿using Exercice01;
+
+namespace Exercice01Test
+{
+    [TestClass]
+    public sealed class GradingCalculatorTest
+    {
+        [TestMethod]
+        public void WhenGetGrade_95_90_Then_A()
+        {
+            GradingCalculator gradingCalculator = new GradingCalculator();
+
+            gradingCalculator.Score = 95;
+            gradingCalculator.AttendancePercentage = 90;
+
+            var result = gradingCalculator.GetGrade();
+
+            Assert.AreEqual('A', result);
+        }
+
+        [TestMethod]
+        public void WhenGetGrade_85_90_Then_B()
+        {
+            GradingCalculator gradingCalculator = new GradingCalculator();
+
+            gradingCalculator.Score = 85;
+            gradingCalculator.AttendancePercentage = 90;
+
+            var result = gradingCalculator.GetGrade();
+
+            Assert.AreEqual('B', result);
+        }
+
+        [TestMethod]
+        public void WhenGetGrade_65_90_Then_C()
+        {
+            GradingCalculator gradingCalculator = new GradingCalculator();
+
+            gradingCalculator.Score = 65;
+            gradingCalculator.AttendancePercentage = 90;
+
+            var result = gradingCalculator.GetGrade();
+
+            Assert.AreEqual('C', result);
+        }
+
+        [TestMethod]
+        public void WhenGetGrade_95_65_Then_B()
+        {
+            GradingCalculator gradingCalculator = new GradingCalculator();
+
+            gradingCalculator.Score = 95;
+            gradingCalculator.AttendancePercentage = 65;
+
+            var result = gradingCalculator.GetGrade();
+
+            Assert.AreEqual('B', result);
+        }
+
+        [TestMethod]
+        public void WhenGetGrade_95_55_Then_F()
+        {
+            GradingCalculator gradingCalculator = new GradingCalculator();
+
+            gradingCalculator.Score = 95;
+            gradingCalculator.AttendancePercentage = 55;
+
+            var result = gradingCalculator.GetGrade();
+
+            Assert.AreEqual('F', result);
+        }
+
+        [TestMethod]
+        public void WhenGetGrade_65_55_Then_F()
+        {
+            GradingCalculator gradingCalculator = new GradingCalculator();
+
+            gradingCalculator.Score = 65;
+            gradingCalculator.AttendancePercentage = 55;
+
+            var result = gradingCalculator.GetGrade();
+
+            Assert.AreEqual('F', result);
+        }
+
+        [TestMethod]
+        public void WhenGetGrade_50_90_Then_F()
+        {
+            GradingCalculator gradingCalculator = new GradingCalculator();
+
+            gradingCalculator.Score = 50;
+            gradingCalculator.AttendancePercentage = 90;
+
+            var result = gradingCalculator.GetGrade();
+
+            Assert.AreEqual('F', result);
+        }
+
+    }
+}
