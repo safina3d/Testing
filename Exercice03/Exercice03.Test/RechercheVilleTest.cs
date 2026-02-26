a5b5c86f05c6cdc879e9519c65c16710ad1e306f
Zakaria Z. <zakaria@m2i.com>
2026-02-26 10:51:48 +0100

    ajout test Si le texte de la recherche contient moins de 2 caractères

--- Exercice03/Exercice03.Test/RechercheVilleTest.cs
+++ Exercice03/Exercice03.Test/RechercheVilleTest.cs
@@ -3,8 +3,20 @@ namespace Exercice03.Test;
 [TestClass]
 public class RechercheVilleTest
 {
-    [TestMethod]
-    public void TestMethod1()
+    private RechercheVille? _rechercheVille;
+    
+    public void Setup()
     {
+        _rechercheVille = new RechercheVille();
     }
+
+    [TestMethod]
+    public void WhenRechercher_WithLessThan2Chars_ThenException()
+    {
+        Setup();
+        Assert.Throws<NotFoundException>(() => _rechercheVille?.Rechercher("A"));
+    }
+
+
+
 }
