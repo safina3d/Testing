bbb5e061616d395fbe0b1cbf97ca25fd2422dad7
Zakaria Z. <zakaria@m2i.com>
2026-02-26 11:20:02 +0100

    Ajout de la recherche n'est qu'une partie d'un nom de ville

--- Exercice03/Exercice03/RechercheVille.cs
+++ Exercice03/Exercice03/RechercheVille.cs
@@ -17,7 +17,7 @@ public class RechercheVille
         if (mot.Length < 2) throw new NotFoundException("Le nom de la ville doit contenir au moins 2 caractères");
 
         mot = mot.Trim().ToLower();
-        var filteredCities = _villes.Where(city => city.StartsWith(mot)).ToList();
+        var filteredCities = _villes.Where(city => city.Contains(mot)).ToList();
         return filteredCities;
     }
 
